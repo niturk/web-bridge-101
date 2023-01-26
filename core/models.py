6cd343deca91ff9c2691ea7ea85fdbbcfe68ce2e
Nikita TURKMEN <info@nikitaturkmen.com>
2023-01-26 13:29:01 +0300

    project created.

---
+++ core/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+
+# Create your models here.
+class Message(models.Model):
+    message = models.CharField(max_length=1000)
+    device = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
